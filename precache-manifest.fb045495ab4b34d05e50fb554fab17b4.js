b859f374232cc7de8f1090d6d1269746371963c3
Andrew Sverdrup <team@programmingliftoff.com>
2020-06-21 13:28:41 -0700

    Updates

---
+++ precache-manifest.fb045495ab4b34d05e50fb554fab17b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "213c1594ffdbc5a52a899ce72e00c20d",
+    "url": "/github-pages-create-react-app/index.html"
+  },
+  {
+    "revision": "67df6ddc8751440babc0",
+    "url": "/github-pages-create-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "36fb853b09e8c9b74812",
+    "url": "/github-pages-create-react-app/static/js/2.be7e122f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-pages-create-react-app/static/js/2.be7e122f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67df6ddc8751440babc0",
+    "url": "/github-pages-create-react-app/static/js/main.00d27f48.chunk.js"
+  },
+  {
+    "revision": "d4fdfa876bd1d4cf1eec",
+    "url": "/github-pages-create-react-app/static/js/runtime-main.a2f510c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-pages-create-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
